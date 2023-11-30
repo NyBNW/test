c3fa215eb3b53f0d994502a5ac7e2df4291faa52
NyBNW <152493717+NyBNW@users.noreply.github.com>
2023-11-30 19:22:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,49 @@ st.sidebar.header('Dress Up Game')
 selected_items = st.sidebar.multiselect('Select Clothing Items', clothing_items)
 selected_accessories = st.sidebar.multiselect('Select Accessories', accessories)
 
+# Allow users to upload drawings
+st.sidebar.header('Upload Drawings')
+uploaded_files = st.sidebar.file_uploader('Upload your drawings', accept_multiple_files=True, type=['png', 'jpg'])
+
+# Display the selected clothing items and accessories
+st.header('Selected Items')
+for item in selected_items:
+    st.write(item)
+
+for accessory in selected_accessories:
+    st.write(accessory)
+
+# Display the uploaded drawings
+st.header('Uploaded Drawings')
+for file in uploaded_files:
+    st.image(file)
+
+# Display the virtual character with the selected items and drawings
+st.header('Virtual Character')
+# Add code here to display the virtual character with the selected items and drawingsimport streamlit as st
+
+# Define a list of available clothing items and accessories
+clothing_items = [
+    'T-Shirt',
+    'Dress',
+    'Jeans',
+    'Skirt',
+    'Jacket'
+]
+
+accessories = [
+    'Hat',
+    'Sunglasses',
+    'Necklace',
+    'Handbag',
+    'Shoes'
+]
+
+# Create a sidebar section for selecting clothing items and accessories
+st.sidebar.header('Dress Up Game')
+selected_items = st.sidebar.multiselect('Select Clothing Items', clothing_items)
+selected_accessories = st.sidebar.multiselect('Select Accessories', accessories)
+
 # Display the selected clothing items and accessories
 st.header('Selected Items')
 for item in selected_items:
